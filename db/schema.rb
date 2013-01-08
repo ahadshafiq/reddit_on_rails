6196396ce23398a126a8494b38c32b812d7b7897
ahadshafiq <ahad.shafiq@gmail.com>
2013-01-08 18:58:09 +0300

    Added has many and belongs to relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130108150034) do
+ActiveRecord::Schema.define(:version => 20130108154008) do
+
+  create_table "links", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "title"
+  end
 
   create_table "users", :force => true do |t|
     t.datetime "created_at",                             :null => false
