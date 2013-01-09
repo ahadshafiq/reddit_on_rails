7fcea4dd0fff63ce094557fcfa945763193276a5
ahadshafiq <ahad.shafiq@gmail.com>
2013-01-09 13:25:32 +0300

    Added links to the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
 
 	def index
+		@links = Link.all
+
+	    respond_to do |format|
+	      format.html # index.html.erb
+	      format.json { render json: @links }
+	    end
 	end
 	
 end
