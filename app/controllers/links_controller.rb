e6304b6b705cb5e56b95040f4b3ef4e597c6674d
ahadshafiq <ahad.shafiq@gmail.com>
2013-01-11 18:42:51 +0300

    added commenting

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,10 +1,13 @@
 class LinksController < ApplicationController
 
 	def show
+		@link    = Link.find(params[:id])
+		@comment = Comment.new
 	end
 	
 	def new
 		@link = Link.new
+	  
 	end
 
 	def create
