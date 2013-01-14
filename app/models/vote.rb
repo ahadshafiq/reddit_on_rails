84250e392a7c92daacf7a5033eea55dced330ae6
ahadshafiq <ahad.shafiq@gmail.com>
2013-01-14 20:09:12 +0300

    Completed voting capability

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,7 @@
 class Vote < ActiveRecord::Base
-   attr_accessible :up
+   attr_accessible :up, :link_id
+   validates :user_id, :uniqueness => { :scope => :link_id }
 	belongs_to :user
 	belongs_to :link
+
 end
