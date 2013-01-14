9e63e2b0eeae9123e2b3d0f0ed23e4244a34311a
ahadshafiq <ahad.shafiq@gmail.com>
2013-01-14 19:25:29 +0300

    Added a vote model and associations

---
+++ app/models/vote.rb
@@ -0,0 +1,5 @@
+class Vote < ActiveRecord::Base
+   attr_accessible :up
+	belongs_to :user
+	belongs_to :link
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130111142434) do
+ActiveRecord::Schema.define(:version => 20130114144133) do
 
   create_table "comments", :force => true do |t|
     t.text     "message"
@@ -46,5 +46,13 @@ ActiveRecord::Schema.define(:version => 20130111142434) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.boolean  "up"
+    t.integer  "user_id"
+    t.integer  "link_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
